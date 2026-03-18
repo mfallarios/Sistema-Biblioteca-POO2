dc547bcfe2915b6c5b8b5302161e5b650c3e8322
Margiori Marilesi Falla Rios <mfallarios@gmail.com>
2026-03-17 21:54:17 -0500

    primer commit: estructura base limpia

---
+++ src/main/java/com/mfallarios/sistemabibliotecapoo2/SistemaBibliotecaPoo2Application.java
@@ -0,0 +1,13 @@
+package com.mfallarios.sistemabibliotecapoo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaBibliotecaPoo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaBibliotecaPoo2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/mfallarios/sistemabibliotecapoo2/SistemaBibliotecaPoo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfallarios.sistemabibliotecapoo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBibliotecaPoo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
